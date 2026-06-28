4ae6942f74d53b01d5cef5dcff857caf3c00700f
Ankit <adhikariankit8@gmail.com>
2026-06-29 00:26:52 +0545

    changed simething necessary

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,21 @@
-export { default } from "./App.jsx";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="bg-gray-900 text-white min-h-screen">
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
